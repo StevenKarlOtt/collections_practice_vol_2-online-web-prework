49e9cfdbd847289b4006ca7e4e5c478a05994e2b
Steven Ott <stevenkott@icloud.com>
2018-11-25 02:24:23 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,4 +1,12 @@
 def begins_with_r(letters)
-  letters.all?{ |words| words.starts_with?("r") }
+  letters.all?{ |words| words.start_with?("r") }
+end
+
+def contain_a(letters)
+  letters.find_all {|lett| lett.include?("a")}
+end
+
+def first_wa(letters)
+  letters.find { |lett| lett.include?("wa")}
 end
   
